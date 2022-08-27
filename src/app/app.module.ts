beaa519b5ef8ab5aa95805a4b6fa71f7a0794ee6
Artx <arrtxweb@gmail.com>
2022-08-26 22:36:32 -0300

    test - angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { AddProductComponent } from './pages/add-product/add-product.component';
+import { UpdateProductComponent } from './pages/update-product/update-product.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgetPasswordComponent,
+    ProfileComponent,
+    AddProductComponent,
+    UpdateProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
