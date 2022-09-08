c69489ea3f7315c2576ac1c4349ef65dea20fc26
Artx <arrtxweb@gmail.com>
2022-09-08 20:31:43 -0300

    tela de perfil

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  styles: [
+    `
+      :host ::ng-deep .p-password input {
+        width: 400px;
+        height: 50px;
+        padding: 5px;
+
+        outline: 0;
+        border-radius: 10px;
+        border-style: none;
+        margin-bottom: 15px;
+        align-items: center;
+        background-color: #161616;
+      }
+    `,
+  ],
 })
 export class ProfileComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  value1!: string;
 
-  ngOnInit(): void {
-  }
+  value2!: string;
 
+  value3!: string;
+
+  value4!: string;
+
+  value5!: string;
+
+  value6!: string;
 }
