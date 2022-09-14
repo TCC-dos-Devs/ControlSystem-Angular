bcd316572d3e0508db2271ffa9ab48684e2bf773
Marcos William <mwdvweb@gmail.com>
2022-09-13 22:51:45 -0300

    Ajeitando tela de cadastro

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -11,5 +11,11 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  value1!: string;
 
+  value2!: string;
+  value3!: string;
+  value4!: string;
+  handleClick(){}
 }
+
