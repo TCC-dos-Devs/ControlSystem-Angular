2f5cb725f46a5830d9fc1086ed42fcd74db23eea
Artx <arrtxweb@gmail.com>
2022-09-13 22:54:29 -0300

    tela de login

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleClick() {
+    this.router.navigateByUrl('/');
+  }
+  cancelClick() {
+    this.router.navigateByUrl('/');
   }
 
+  value1!: string;
+  value2!: string;
 }
